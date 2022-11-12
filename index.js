cae809986d815db3a75a691a68489645faef7204
lxzlxn <117989584+lxzlxn@users.noreply.github.com>
2022-11-12 09:16:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||李雨萱|祝|你|生|日|快|乐|作者|吕相志|🎂');
+      S.UI.simulate('|#countdown 3||李雨萱|祝|你|生|日|快|乐|By:吕相志|🎂');
     }
 
     S.Drawing.loop(function () {
